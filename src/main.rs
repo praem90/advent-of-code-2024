f75abad86c84d0e1442dd389ceeb85a49d36022f
Mohan Raj <praem1990@gmail.com>
2024-12-12 01:49:33 +0530

    Day 2 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::env::args;
 use std::io::Read;
 use std::fs::File;
 
@@ -8,48 +7,59 @@ fn main() {
     let mut content = String::new();
     input_file.read_to_string(&mut content).unwrap();
 
-    let mut loc_a = vec![];
-    let mut loc_b = vec![];
+    let mut safe_reports = 0;
 
     content.split("\n").for_each(|l| {
-        let mut line = l.split_whitespace();
+        if l.trim().len() == 0{
+            return;
+        }
+        println!("The report {}", l);
+        let levels: Vec<i32> = l.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
+        let mut prev = None;
+        let mut is_asc = None;
 
-        if let Some(a) = line.next() {
-            loc_a.push(a.parse::<i32>().unwrap())
+        let mut valid = true;
+
+        for l in levels {
+            println!("The level {}", l);
+            if let Some(p) = prev {
+                let diff: i32 = l - p;
+                println!("l={}, p={}, diff={}", l, p, diff);
+                if let None = is_asc {
+                    is_asc = Some(diff > 0)
+                }
+
+                if diff == 0 {
+                    valid = false;
+                    break;
+                }
+
+                if is_asc.unwrap() && diff < 0 {
+                    valid = false;
+                    break;
+                }
+
+                if !is_asc.unwrap() && diff > 0 {
+                    valid = false;
+                    break;
+                }
+
+                if diff.abs() > 3 {
+                    valid = false;
+                    break;
+                }
+
+            }
+            prev = Some(l);
         }
 
-        if let Some(b) = line.next() {
-            loc_b.push(b.parse::<i32>().unwrap())
+        if valid {
+            safe_reports += 1;
+            println!("Valid Report");
+        } else {
+            println!("invalid report");
         }
     });
 
-    let mut length: i32 = 0;
-    let args_count = args().len();
-
-    if args_count == 1 {
-        loc_a.sort();
-        loc_b.sort();
-
-        for i in 0..loc_a.len() {
-            let diff = loc_a.get(i).unwrap() - loc_b.get(i).unwrap();
-            length = length + diff.abs();
-
-            println!("l1={}, l2={}, diff={}, length={}",
-                loc_a.get(i).unwrap(),
-                loc_b.get(i).unwrap(),
-                diff.abs(),
-                length);
-        }
-
-
-    } else {
-        for i in 0..loc_a.len() {
-            let value = loc_a.get(i).unwrap();
-            let n = loc_b.iter().filter(|&v| v == value).count() as i32;
-
-            length = length + (value * n);
-        }
-    }
-
-    println!("The second length is {}", length);
+    println!("The no of safe reports {}", safe_reports);
 }
